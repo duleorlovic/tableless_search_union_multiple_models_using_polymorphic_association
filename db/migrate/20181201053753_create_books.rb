5a3aacd3a4d11450b92d5c21715064d931ab8b29
Dusan Orlovic <duleorlovic@gmail.com>
2018-12-01 06:41:23 +0100

    Edit migration column created_by to point to users table
    vi db/migrate/20181201053753_create_books.rb
    t.references :created_by, foreign_key: { to_table: :users }
    
    rake db:setup

--- db/migrate/20181201053753_create_books.rb
+++ db/migrate/20181201053753_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :title
       t.string :pdf_download_url
-      t.references :created_by, foreign_key: true
+      t.references :created_by, foreign_key: { to_table: :users }
 
       t.timestamps
     end
